8c13a80410e9e6c2e32fc9aeca856bc0730bc2b8
satyajit nayak <satyajitn15@gmail.com>
2024-01-07 21:20:04 +0530

    add config.go to read configuration

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.20
 
 require go.uber.org/zap v1.26.0
 
-require go.uber.org/multierr v1.10.0 // indirect
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1
+)
--- utils/config.go
+++ utils/config.go
@@ -1,5 +1,12 @@
 package utils
 
+import (
+	"errors"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
 type LBStrategy int
 
 // define enum
@@ -15,4 +22,38 @@ func GetLBStrategy(strategy string) LBStrategy {
 	default:
 		return RoundRobin
 	}
+}
+
+type Config struct {
+	Port           int      `yaml:"lb_port"`
+	MaxAttempLimit int      `yaml:"max_attempt_limit"`
+	Backends       []string `yaml:"backends"`
+	Strategy       string   `yaml:"strategy"`
+}
+
+const MAX_LB_ATTEMPTS int = 3
+
+// reads and parses the load balancer configuration from the "config.yaml" file.
+// It returns a pointer to a Config struct representing the parsed configuration.
+func GetLBConfig() (*Config, error) {
+	var config Config
+	configFile, err := os.ReadFile("config.yaml")
+	if err != nil {
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(config.Backends) == 0 {
+		return nil, errors.New("backend host expected, none provided")
+	}
+
+	if config.Port == 0 {
+		return nil, errors.New("load balancer port not found")
+	}
+
+	return &config, nil
 }
